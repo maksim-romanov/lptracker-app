5f7eededee15fb4b68899144adbf91d4bfb0fda7
Maksim Romanov <romanov.maxim.98@gmail.com>
2026-06-05 13:13:35 +0300

    feat(server): route TOKEN_PRICE_SERVICE through tokens-data adapter

--- apps/server/src/features/token-prices/di/register.ts
+++ apps/server/src/features/token-prices/di/register.ts
@@ -1,8 +1,8 @@
 import { container } from "tsyringe";
 
-import { TokenPriceCache } from "../data/token-price.cache";
+import { TokensDataPriceAdapter } from "../../../shared/clients/tokens-data/price-adapter";
 import { TOKEN_PRICE_SERVICE } from "./tokens";
 
 export function register() {
-  container.register(TOKEN_PRICE_SERVICE, { useToken: TokenPriceCache });
+  container.register(TOKEN_PRICE_SERVICE, { useToken: TokensDataPriceAdapter });
 }
